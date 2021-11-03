6ed291e09bbaed60f8264d6d3d3d492f9304398c
eldormero <martin.coulon@epita.fr>
2021-11-03 21:56:19 +0100

    Add possibility to get basic infos

---
+++ infos.py
@@ -0,0 +1,4 @@
+def get_infos():
+    print("Users are listed in 'users.csv' file")
+    print("Expenses are listed in 'expense_report.csv' file")
+    return True
--- main.py
+++ main.py
@@ -3,13 +3,14 @@ from examples import custom_style_2
 from expense import expense_questions,new_expense
 from status import get_status
 from user import add_user
+from infos import get_infos
 
 def ask_option():
     main_option = {
         "type":"list",
         "name":"main_options",
         "message":"Expense Tracker v0.1",
-        "choices": ["New Expense","Show Status","New User"]
+        "choices": ["New Expense","Show Status","New User","Infos"]
     }
     option = prompt(main_option)
     if (option['main_options']) == "New Expense":
@@ -20,6 +21,9 @@ def ask_option():
         ask_option()
     if (option['main_options']) == "Show Status":
         get_status()
+        ask_option()
+    if (option['main_options']) == "Infos":
+        get_infos()
         ask_option()
 
 def main():
