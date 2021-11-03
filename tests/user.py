bbfbcdfe2d1d0438125400853dd973b76b74c9a2
eldormero <martin.coulon@epita.fr>
2021-11-03 21:24:34 +0100

    Add correctly user to csv

--- tests/user.py
+++ tests/user.py
@@ -5,4 +5,5 @@ class TestUser(unittest.TestCase):
 
     def test_add_user(self):
         self.assertEqual(add_user('bonjour'), True)
+        self.assertEqual(add_user('Robin'), True)
         self.assertEqual(add_user(42), False)
--- user.py
+++ user.py
@@ -1,4 +1,6 @@
 from PyInquirer import prompt
+import csv
+
 user_questions = [
     {
         "type":"input",
@@ -7,8 +9,21 @@ user_questions = [
     },
 ]
 
+# Add users to a csv file (users.csv)
+def add_to_csv(*args):
+    with open('users.csv', 'a', newline='') as csvfile:
+        user = []
+        infos = args[0]
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quoting=csv.QUOTE_MINIMAL)
+        for e in infos:
+            user.append(infos[e])
+        spamwriter.writerow([user[0]])
+
 def add_user():
     # This function should create a new user, asking for its name
     infos = prompt(user_questions)
-    print("User Added !")
+    # Add user to CSV
+    add_to_csv(infos)
+    print("New User Added !")
     return True
