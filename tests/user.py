878b2781cd248895a201977b49f1255002759248
eldormero <martin.coulon@epita.fr>
2021-11-03 20:31:58 +0100

    Add user test

---
+++ tests/user.py
@@ -0,0 +1,8 @@
+import unittest
+from user import add_user
+
+class TestUser(unittest.TestCase):
+
+    def test_add_user(self):
+        self.assertEqual(add_user('bonjour'), True)
+        self.assertEqual(add_user(42), False)
