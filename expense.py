90c8e7e3b1da5dc64ec65d0307d1b7efd2922afb
eldormero <martin.coulon@epita.fr>
2021-11-03 20:39:35 +0100

    Select user and add expense to csv

--- expense.py
+++ expense.py
@@ -1,4 +1,15 @@
 from PyInquirer import prompt
+import csv
+
+
+# Get user from the csv file users.csv and return a list of users
+def get_user():
+    with open('users.csv', newline='') as f:
+        reader = csv.reader(f)
+        l = []
+        for row in reader:
+            l.append(row[0])
+        return l
 
 expense_questions = [
     {
@@ -12,19 +23,26 @@ expense_questions = [
         "message":"New Expense - Label: ",
     },
     {
-        "type":"input",
+        'type': 'list',
         "name":"spender",
         "message":"New Expense - Spender: ",
+        # Select a user between the different existant user in the users.csv file
+        "choices": get_user()
     },
 
 ]
 
 
 
+def add_to_csv(*args):
+    with open('expense_report.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        writer.writerows(args)
+
 def new_expense(*args):
     infos = prompt(expense_questions)
     # Writing the informations on external file might be a good idea ¯\_(ツ)_/¯
-    print("Expense Added !")
+    add_to_csv(infos)
     return True
 
 
